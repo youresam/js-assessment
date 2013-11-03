dcd0a8540991e7350460a172f42f36368b20f3a2
Sam Messina <sam.messina@gmail.com>
2013-11-03 15:33:47 -0600

    BEST PRACTICES SOLVED AW YEAH

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -8,7 +8,7 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     globals : function() {
-      myObject = {
+      var myObject = {
         name : 'Jory'
       };
 
@@ -16,21 +16,15 @@ define(function() {
     },
 
     functions : function(flag) {
-      if (flag) {
-        function getValue() { return 'a'; }
-      } else {
-        function getValue() { return 'b'; }
-      }
-
-      return getValue();
+      return flag ? 'a' : 'b';
     },
 
     parseInt : function(num) {
-      return parseInt(num);
+      return parseInt(num, 10);
     },
 
     identity : function(val1, val2) {
-
+		return val1 === val2;
     }
   };
 });
