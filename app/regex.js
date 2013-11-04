ce8a9f25a8e650269fbc086cafa184dc666b4747
Sam Messina <sam.messina@gmail.com>
2013-11-03 21:05:08 -0600

    regex is easy

--- app/regex.js
+++ app/regex.js
@@ -3,26 +3,27 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+      return (/\d/).test(str);
     },
 
     containsRepeatingLetter : function(str) {
-
+      return (/([a-z])\1/i).test(str);
     },
 
     endsWithVowel : function(str) {
-
+      return (/[aeiou]$/i).test(str);
     },
 
     captureThreeNumbers : function(str) {
-
+      var match = str.match(/\d{3}/);
+      return (match === null) ? false : match[0];
     },
 
     matchesPattern : function(str) {
-
+      return (/^\d{3}-\d{3}-\d{4}$/).test(str);
     },
     isUSD : function(str) {
-
+      return (/^\$\d{1,3}(,\d{3})*(\.\d{2})?$/).test(str);
     }
   };
 });
