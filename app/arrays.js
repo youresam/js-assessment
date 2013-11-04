d828fdd0ba19deebae1bd038582bfa0b93c578f4
Sam Messina <sam.messina@gmail.com>
2013-11-03 18:21:09 -0600

    Reformatting tabs, just realized source files were formatted with 2 spaces instead of 4

--- app/arrays.js
+++ app/arrays.js
@@ -7,87 +7,87 @@ define(function() {
     },
 
     sum : function(arr) {
-        var summation = 0;
-        for(var i in arr) {
-            summation += arr[i];
-        }
-        return summation;
+      var summation = 0;
+      for(var i in arr) {
+        summation += arr[i];
+      }
+      return summation;
     },
 
     remove : function(arr, item) {
-        return this.removeWithoutCopy(arr.slice(), item);
+      return this.removeWithoutCopy(arr.slice(), item);
     },
 
     removeWithoutCopy : function(arr, item) {
-        for(var i=arr.length-1; i>=0; i--) {
-            if(arr[i] === item) {
-                arr.splice(i, 1);
-            }
+      for(var i = arr.length - 1; i >= 0; i--) {
+        if(arr[i] === item) {
+          arr.splice(i, 1);
         }
-        return arr;
+      }
+      return arr;
     },
 
     append : function(arr, item) {
-        return arr.concat(item);
+      return arr.concat(item);
     },
 
     truncate : function(arr) {
-        return arr.slice(0, arr.length - 1);
+      return arr.slice(0, arr.length - 1);
     },
 
     prepend : function(arr, item) {
-        arr.unshift(item);
-        return arr;
+      arr.unshift(item);
+      return arr;
     },
 
     curtail : function(arr) {
-        return arr.slice(1);
+      return arr.slice(1);
     },
 
     concat : function(arr1, arr2) {
-        return arr1.concat(arr2);
+      return arr1.concat(arr2);
     },
 
     insert : function(arr, item, index) {
-        arr.splice(index, 0, item);
-        return arr;
+      arr.splice(index, 0, item);
+      return arr;
     },
 
     count : function(arr, item) {
-        var occurrences = 0;
-        for(var i in arr) {
-            if(arr[i] === item) {
-                occurrences++;
-            }
+      var occurrences = 0;
+      for(var i in arr) {
+        if(arr[i] === item) {
+          occurrences++;
         }
-        return occurrences;
+      }
+      return occurrences;
     },
 
     duplicates : function(arr) {
-        var dupes = [];
-        for(var i in arr) {
-            if(arr.indexOf(arr[i]) < i && dupes.indexOf(arr[i]) === -1) {
-                dupes.push(arr[i]);
-            }
+      var dupes = [];
+      for(var i in arr) {
+        if(arr.indexOf(arr[i]) < i && dupes.indexOf(arr[i]) === -1) {
+          dupes.push(arr[i]);
         }
-        return dupes;
+      }
+      return dupes;
     },
 
     square : function(arr) {
-        for(var i in arr) {
-            arr[i] *= arr[i];
-        }
-        return arr;
+      for(var i in arr) {
+        arr[i] *= arr[i];
+      }
+      return arr;
     },
 
     findAllOccurrences : function(arr, target) {
-        var occurrences = [];
-        for(var i in arr) {
-            if(arr[i] === target) {
-                occurrences.push(i);
-            }
+      var occurrences = [];
+      for(var i in arr) {
+        if(arr[i] === target) {
+          occurrences.push(i);
         }
-        return occurrences;
+      }
+      return occurrences;
     }
   };
 });
--- app/objects.js
+++ app/objects.js
@@ -3,21 +3,21 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-        return fn.call(obj);
+      return fn.call(obj);
     },
 
     alterObjects : function(constructor, greeting) {
-        constructor.prototype.greeting = greeting;
+      constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
-        var result = [];
-        for(var i in obj) {
-            if(obj.hasOwnProperty(i)) {
-                result.push(i + ': ' + obj[i]);
-            }
+      var result = [];
+      for(var i in obj) {
+        if(obj.hasOwnProperty(i)) {
+          result.push(i + ': ' + obj[i]);
         }
-        return result;
+      }
+      return result;
     }
   };
 });
