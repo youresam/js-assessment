e9a459260c6afa8c22ca45df61d147a5275ed62e
Sam Messina <sam.messina@gmail.com>
2013-11-03 23:20:28 -0600

    Counting works!

--- app/count.js
+++ app/count.js
@@ -3,6 +3,20 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+      var nextNum = start;
+      var step = function() {
+        console.log(nextNum++);
+        if(nextNum > end) {
+          clearInterval(interval);
+        }
+      };
+      var interval = setInterval(step, 100);
+      step();
+      return {
+        cancel: function() {
+          clearInterval(interval);
+        }
+      };
     }
   };
 });
